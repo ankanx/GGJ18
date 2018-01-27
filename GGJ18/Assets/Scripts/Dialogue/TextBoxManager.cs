d4f41e3899d70f69dc42b21381edae8151f70926
Sebastian Fransson <Sebastianlfransson@gmail.com>
2018-01-27 22:50:10 +0100

    Scripts

--- GGJ18/Assets/Scripts/Dialogue/TextBoxManager.cs
+++ GGJ18/Assets/Scripts/Dialogue/TextBoxManager.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextBoxManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public GameObject textBox;
+    public Text theText;
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int currentLine;
+    public int endAtLine;
+
+
+    // Use this for initialization
+    void Start () {
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
