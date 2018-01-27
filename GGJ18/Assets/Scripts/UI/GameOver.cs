99bb6ca2304afb571e5fd8b13c72690d0d2499e4
Pontus <pontus.laestadius@gmail.com>
2018-01-27 18:37:25 +0100

    adds simple stuff

---
+++ GGJ18/Assets/Scripts/UI/GameOver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+	
+	public void Quit() {
+		Application.Quit();
+	}
+	
+	public void Retry () {
+		Debug.Log("Retry");
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
+	public void Menu() {
+		Debug.Log("TODO Menu() in GameOver script.");
+		 //SceneManager.LoadScene("Menues");
+	}
+
+}
