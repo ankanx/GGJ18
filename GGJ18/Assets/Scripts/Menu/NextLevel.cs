fd2b41715d2cf213ff9fac39d6f01b083141e375
Tim Jonasson <gusjonati@student.gu.se>
2018-01-27 18:38:39 +0100

    Next level menu

---
+++ GGJ18/Assets/Scripts/Menu/NextLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevel : MonoBehaviour {
+
+	public GameObject menu;
+
+	public void Activate()
+	{
+		//Next level stuff
+	}
+}
