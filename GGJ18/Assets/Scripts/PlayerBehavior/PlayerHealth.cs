5dd66b31a38eadd1b088b31e985f21c5a7de6288
Pontus <pontus.laestadius@gmail.com>
2018-01-27 17:56:38 +0100

    adds prefabs and stuff

--- GGJ18/Assets/Scripts/PlayerBehavior/PlayerHealth.cs
+++ GGJ18/Assets/Scripts/PlayerBehavior/PlayerHealth.cs
@@ -1,29 +1,76 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System;
+
 
 public class PlayerHealth : MonoBehaviour
 {
     public int startingHealth = 100;                            // The amount of health the player starts the game with.
     public int currentHealth;                                   // The current health the player has.
     public Slider healthSlider;                                 // Reference to the UI's health bar.
-
+    public int takeDamage = 25;
+    private long msec = 0;
+    private DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+    private bool damaged = false;
+	GameObject go;
+    
     void Awake ()
     {
+	 go = GameObject.Find("gameover");
+	go.SetActive(false);
         currentHealth = startingHealth;
+	TakeDamage(0);
+        msec = 0;
+    }
+
+    void respawn() {
+	currentHealth = startingHealth;
+	TakeDamage(0);
+        msec = 0;
     }
 
     void Update ()
     {
-
+	if (damaged) {
+		this.TakeDamage(takeDamage);	
+	}
     }
 
     public void TakeDamage (int amount)
-    { // Reduce the current health by the damage amount.
+    {
+	long now = Convert.ToInt64((DateTime.Now - epoch).TotalMilliseconds);
+	if (msec + 500 > now)
+		return;
+	msec = now;
         currentHealth -= amount;
 
-        // Set the health bar's value to the current health.
-        healthSlider.value = currentHealth;
-
+	if (currentHealth < 0) {
+		Death();
+	}
+	healthSlider.value = currentHealth; 
+	this.damaged = false;
     }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Enemy" || coll.gameObject.tag == "deadly") {
+		this.damaged = true;
+		this.TakeDamage(takeDamage);
+	}
+    }
+
+	void OnCollisionExit2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Enemy" || coll.gameObject.tag == "deadly") {
+		this.damaged = false;
+
+	}
+    }
+
+       public void Death() {
+	gameObject.GetComponent<InputScript>().enabled = false;
+	go.SetActive(true);
+    }
+
 }
---
+++ GGJ18/Assets/Scripts/UI/GameOver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+	
+	public void Quit() {
+		Application.Quit();
+	}
+	
+	public void Retry () {
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
+	public void Menu() {
+		Debug.Log("TODO Menu() in GameOver script.");
+		 //SceneManager.LoadScene("Menues");
+	}
+
+}
