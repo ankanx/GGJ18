5eaed9e7c0d0e2b412dbbac085e84aa772c49056
Pontus <pontus.laestadius@gmail.com>
2018-01-27 18:43:27 +0100

    fixes hiding bug

--- GGJ18/Assets/Scripts/UI/GameOver.cs
+++ GGJ18/Assets/Scripts/UI/GameOver.cs
@@ -6,16 +6,16 @@ using UnityEngine.SceneManagement;
 public class GameOver : MonoBehaviour {
 	
 	public void Quit() {
-		Application.Quit();
+		//Application.Quit();
 	}
 	
 	public void Retry () {
-		Debug.Log("Retry");
-		Application.LoadLevel(Application.loadedLevel);
+		//Debug.Log("Retry");
+		//Application.LoadLevel(Application.loadedLevel);
 	}
 
 	public void Menu() {
-		Debug.Log("TODO Menu() in GameOver script.");
+		//Debug.Log("TODO Menu() in GameOver script.");
 		 //SceneManager.LoadScene("Menues");
 	}
 
