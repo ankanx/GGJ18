e71f0d259e8bb0ed6f9cf35d6bb9e114079aaa41
Ankanx <ankanx@gmail.com>
2018-01-27 01:56:29 +0100

    Fixed a lot

--- GGJ18/Assets/Scripts/Menu/SelectOnInput.cs
+++ GGJ18/Assets/Scripts/Menu/SelectOnInput.cs
@@ -1,16 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class SelectOnInput : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+
+    private bool buttonSelected;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Vertical") != 0 && buttonSelected == false)
+        {
+            eventSystem.SetSelectedGameObject(selectedObject);
+            buttonSelected = true;
+        }
+    }
+
+    private void OnDisable()
+    {
+        buttonSelected = false;
+    }
 }
