1e0bc9ba2175ac48663890cbbf64f82abf9bcde7
Ankanx <ankanx@gmail.com>
2018-01-28 15:34:28 +0100

    update

---
+++ GGJ18/Assets/rng.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rng : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //gameObject.GetComponent<SpriteRenderer>().color = new Color(Random.Range(0, 250), Random.Range(0, 250), Random.Range(0, 250), Random.Range(0, 250));
+	}
+}
