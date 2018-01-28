54e958d00a05128e9def303f91f3dd98b63dcc94
Sebastian Fransson <Sebastianlfransson@gmail.com>
2018-01-28 02:00:55 +0100

    Text is here

--- GGJ18/Assets/Scripts/Dialogue/TextBoxManager.cs
+++ GGJ18/Assets/Scripts/Dialogue/TextBoxManager.cs
@@ -54,7 +54,7 @@ public class TextBoxManager : MonoBehaviour {
         }
 
         if(currentLine > endAtLine){
-            textBox.SetActive(false);
+            DisableTextBox();
         }
 	}
 
